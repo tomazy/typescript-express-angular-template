394fbc5623ffe0f603c8d53d67e44c2df996e81d
tomazy <tomazy@go2.pl>
2017-03-23 15:35:52 +0800

    Fixed AppComponent tests

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,14 +1,21 @@
+import { Component } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
-
 import { AppComponent } from './app.component';
+
+@Component({
+  selector: 'app-todos',
+  template: '<div>dummy todos</div>'
+})
+class DummyTodosComponent {}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        DummyTodosComponent,
       ],
-    }).compileComponents();
+    });
   }));
 
   it('should create the app', async(() => {
@@ -17,16 +24,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it('should render todos', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toContain('My first');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('My first');
+    expect(compiled.textContent).toContain('dummy todos');
   }));
 });
