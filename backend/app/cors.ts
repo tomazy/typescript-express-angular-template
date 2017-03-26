4a234306cfed87140097b94f1118f353e3a73a7d
tomazy <tomazy@go2.pl>
2017-03-26 17:36:02 +0800

    Fixed cors for requests coming from the same origin

--- backend/app/cors.ts
+++ backend/app/cors.ts
@@ -7,7 +7,11 @@ const whitelist = config.production
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (!origin) {
+      // the call comes from the same origin as the server
+      callback(null, true)
+    }
+    else if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
