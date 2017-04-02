7709f2e2ca9e52472b5f867e071c83265830c6cf
tomazy <tomazy@go2.pl>
2017-04-03 00:52:01 +0800

    Don't wait for selenium if not necessary

--- scripts/wait-for-selenium.sh
+++ scripts/wait-for-selenium.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+if [ -z "$SELENIUM_ADDRESS"]; then exit 0; fi
+
 echo "Waiting for selenium... (${SELENIUM_ADDRESS})"
+
 
 until $(curl --output /dev/null --silent --head --fail $SELENIUM_ADDRESS); do
 	printf '.'
